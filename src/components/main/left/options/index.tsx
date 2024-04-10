7f4d7f2716d0251628c72929e8279c9d337c96da
hvoking <gus@ugeom.com>
2024-04-10 11:53:45 -0400

    testing images alignment

--- src/components/main/left/options/index.tsx
+++ src/components/main/left/options/index.tsx
@@ -6,6 +6,7 @@ import { useFilters } from '../../context/filters'
 
 export const Options = () => {
 	const { equation, setEquation } = useFilters();
+	const path = process.env.PUBLIC_URL;
 
 	return (
 		<div>
@@ -13,31 +14,65 @@ export const Options = () => {
 				Formula Selection
 			</div>
 		    <div className="formulas-wrapper">
-		          <div 
+		          <div
+		          	className="formulas-image-wrapper" 
 		          	onClick={() => setEquation("Sphere")}
-		          	style={{backgroundColor: equation === "Sphere" ? "rgba(0, 0, 0, 1)" : "rgba(0, 120, 120, 0)"}}
+		          	style={{
+		          		backgroundColor: equation === "Sphere" ? 
+		          		"rgba(0, 0, 0, 1)" : 
+		          		"rgba(0, 120, 120, 0)"
+		          	}}
 		          >
-		          	<img className="geometries-selector-wrapper" src={process.env.PUBLIC_URL + "/static/geometries/Asset1.png"} alt="asset-1"/>
+		          	<img 
+		          		className="formulas-image" 
+		          		src={path + "/static/geometries/Asset1.png"} 
+		          		alt="asset-1"
+		          	/>
 		          </div>
 		          <div 
+		          	className="formulas-image-wrapper" 
 		          	onClick={() => setEquation("Cube")}
-      	          	style={{backgroundColor: equation === "Cube" ? "rgba(0, 0, 0, 1)" : "rgba(0, 120, 120, 0)"}}
+      	          	style={{
+      	          		backgroundColor: equation === "Cube" ? 
+      	          		"rgba(0, 0, 0, 1)" : 
+      	          		"rgba(0, 120, 120, 0)"
+      	          	}}
 		          >
-		          	<img className="geometries-selector-wrapper" src={process.env.PUBLIC_URL + "/static/geometries/Asset2.png"} alt="asset-2"/>
+		          	<img 
+		          		className="formulas-image" 
+		          		src={path + "/static/geometries/Asset2.png"} 
+		          		alt="asset-2"
+		          	/>
 		          </div>
 		          <div 
+		          	className="formulas-image-wrapper" 
 		          	onClick={() => setEquation("Cylinder")}
-      	          	style={{backgroundColor: equation === "Cylinder" ? "rgba(0, 0, 0, 1)" : "rgba(0, 120, 120, 0)"}}
+      	          	style={{
+      	          		backgroundColor: equation === "Cylinder" ? 
+      	          		"rgba(0, 0, 0, 1)" : 
+      	          		"rgba(0, 120, 120, 0)"
+      	          	}}
 		          >
-		          	<img className="geometries-selector-wrapper" src={process.env.PUBLIC_URL + "/static/geometries/Asset3.png"} alt="asset-3"/>
+		          	<img 
+		          		className="formulas-image" 
+		          		src={path + "/static/geometries/Asset3.png"} 
+		          		alt="asset-3"
+		          	/>
 		          </div>
 		          <div 
+		          	className="formulas-image-wrapper" 
 		          	onClick={() => setEquation("Equation1")}
       	          	style={{
-      					backgroundColor: equation === "Equation1" ? "rgba(0, 0, 0, 1)" : "rgba(0, 120, 120, 0)"
+      					backgroundColor: equation === "Equation1" ? 
+      					"rgba(0, 0, 0, 1)" : 
+      					"rgba(0, 120, 120, 0)"
       				}}
 		          >
-		          	<img className="geometries-selector-wrapper" src={process.env.PUBLIC_URL + "/static/geometries/Asset4.png"} alt="asset-4"/>
+		          	<img 
+		          		className="formulas-image" 
+		          		src={path + "/static/geometries/Asset4.png"} 
+		          		alt="asset-4"
+		          	/>
 		          </div>
 		     </div>
 		</div>
